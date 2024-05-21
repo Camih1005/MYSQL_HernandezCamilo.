267e53b59ae63e6d10ae28df9e933540350dc67c
Camilo Hernandez <145624469+Camih1005@users.noreply.github.com>
2024-05-20 20:46:05 -0500

    Update permisos.sql

--- dia_9/permisos.sql
+++ dia_9/permisos.sql
@@ -51,3 +51,33 @@ grant select on dia10.* to 'camilo'@'%';
 -- OO
 grant select on dia10.* to 'camilo'@'localhost';
 
+-- dar el tipo de privilegios
+
+flush privileges;
+
+-- revocar los permisos
+
+
+SHOW PROCESSLIST;
+
+kill 163;
+
+-- Conéctate a MySQL como root o un usuario con privilegios suficientes
+mysql -u root -p
+
+-- Revocar los privilegios del usuario
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'usuario'@'localhost';
+FLUSH PRIVILEGES;
+
+-- (Opcional) Eliminar al usuario
+DROP USER 'usuario'@'localhost';
+FLUSH PRIVILEGES;
+
+-- Identificar y terminar todas las conexiones activas del usuario
+SELECT CONCAT('KILL ', Id, ';') FROM INFORMATION_SCHEMA.PROCESSLIST WHERE User='usuario' AND Host='localhost';
+
+-- Ejecutar los comandos KILL generados por la consulta anterior
+-- Aquí suponemos que los IDs obtenidos son 45 y 46
+KILL 45;
+KILL 46;
+
